36b479eb68d22cfde02165d1bd52bd8f3164ebe3
Steve Halliwell <steve@animaginedreality.com>
2023-08-15 08:38:58 +1000

    (#7) Add GroupBy Textures window
    ability to see the textures in use in the scene and some basic info about them

--- Packages/Clarity/Editor/ClarityMenuItems.cs
+++ Packages/Clarity/Editor/ClarityMenuItems.cs
@@ -1,35 +1,10 @@
-﻿using System.Reflection;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEditor;
 
 public static class ClarityMenuItems
 {
     [MenuItem("Edit/Clarity/Find All Lights")]
     public static void FindAllLights()
     {
-        SetSearchFilter("t:light");
-    }
-
-    private static void SetSearchFilter(string filter)
-    {
-        SearchableEditorWindow[] windows = (SearchableEditorWindow[])Resources.FindObjectsOfTypeAll(typeof(SearchableEditorWindow));
-        SearchableEditorWindow hierarchy = null;
-
-        foreach (SearchableEditorWindow window in windows)
-        {
-            if (window.GetType().ToString() == "UnityEditor.SceneHierarchyWindow")
-            {
-                hierarchy = window;
-                break;
-            }
-        }
-
-        if (hierarchy == null)
-            return;
-
-        MethodInfo setSearchType = typeof(SearchableEditorWindow).GetMethod("SetSearchFilter", BindingFlags.NonPublic | BindingFlags.Instance);
-        object[] parameters = new object[] { filter, 0, false, false };
-
-        setSearchType.Invoke(hierarchy, parameters);
+        SearchableEditorWindowUtil.SetSearchFilter("t:light");
     }
 }
---
+++ Packages/Clarity/Editor/GroupByTexturesEditorWindow.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class GroupByTexturesEditorWindow : EditorWindow
+{
+    private Vector2 _scrollPos;
+
+    [MenuItem("Window/Clarity/Group By Textures")]
+    public static void ShowWindow()
+    {
+        var window = GetWindow<GroupByTexturesEditorWindow>();
+        window.titleContent = new GUIContent("Group By Textures");
+        window.Show();
+    }
+
+    public void OnGUI()
+    {
+        _scrollPos = GUILayout.BeginScrollView(_scrollPos);
+        var allRenderables = FindObjectsOfType<Renderer>();
+        GUILayout.Label($"Found {allRenderables.Length} renderables");
+
+        var allMats = allRenderables.SelectMany(x => x.sharedMaterials);
+        var allMatTextures = allMats
+            .SelectMany(mat => GetTexturesFromMat(mat).Select(texture => (mat, texture)))
+            .Where(x => x.texture != null && x.mat != null)
+            .Distinct()
+            .OrderByDescending(x => x.texture.height * x.texture.width);
+
+        var grouped = allMatTextures
+            .GroupBy(x =>
+            {
+                return (x.texture.graphicsFormat, x.texture.height, x.texture.width);
+            });
+
+        GUILayout.Label($"Grouped Texture Height, Texture Width:");
+        foreach (var group in grouped)
+        {
+            var name = group.Key.ToString();
+
+            EditorGUILayout.LabelField(name);
+            EditorGUI.indentLevel++;
+            foreach (var item in group)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.ObjectField(item.texture, typeof(Texture), true);
+                EditorGUILayout.ObjectField(item.mat, typeof(Material), true);
+                if (GUILayout.Button("Isolate"))
+                {
+                    SearchableEditorWindowUtil.SetSearchFilter($"ref:{AssetDatabase.GetAssetPath(item.texture)}");
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+            EditorGUI.indentLevel--;
+        }
+        GUILayout.EndScrollView();
+    }
+
+    private static IEnumerable<Texture> GetTexturesFromMat(Material mat)
+    {
+        if(mat == null)
+        {
+            yield break;
+        }
+
+        var shader = mat.shader;
+        if(shader == null)
+        {
+            yield break;
+        }
+
+        for (int i = 0; i < ShaderUtil.GetPropertyCount(shader); i++)
+        {
+            if (ShaderUtil.GetPropertyType(shader, i) == ShaderUtil.ShaderPropertyType.TexEnv)
+            {
+                Texture texture = mat.GetTexture(ShaderUtil.GetPropertyName(shader, i));
+                yield return texture;
+            }
+        }
+    }
+}
