346da6b3067ff5d323bfd1ca232e5fc3a329640a
Steve Halliwell <steve@animaginedreality.com>
2025-01-24 09:32:13 +1000

    (#19) Add open all scenes menu item

--- Packages/Clarity/Editor/ClarityMenuItems.cs
+++ Packages/Clarity/Editor/ClarityMenuItems.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.Linq;
+using UnityEditor;
 
 public static class ClarityMenuItems
 {
@@ -6,5 +7,24 @@ public static class ClarityMenuItems
     public static void FindAllLights()
     {
         SearchableEditorWindowUtil.SetSearchFilter("t:light");
+    }
+
+    [MenuItem("Edit/Clarity/Open Build Scenes")]
+    public static void OpenBuildScenes()
+    {
+        var scenes = EditorBuildSettings.scenes;
+        scenes = scenes.Where(x => x.guid != default).ToArray();
+        if (scenes.Length == 0)
+        {
+            return;
+        }
+        var zeroScene = scenes[0];
+        var allOtherScenes = scenes.Skip(1).OrderBy(x => x.path);
+        var allScenes = Enumerable.Concat(new[] { zeroScene }, allOtherScenes).ToArray();
+
+        var allScenesInDB = allScenes.Select(x => new UnityEditor.SceneManagement.SceneSetup { path = x.path }).ToArray();
+        allScenesInDB[0].isLoaded = true;
+        allScenesInDB[0].isActive = true;
+        UnityEditor.SceneManagement.EditorSceneManager.RestoreSceneManagerSetup(allScenesInDB);
     }
 }
